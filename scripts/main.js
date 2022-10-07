8a4b6de28b726432d42083138d362bfa3163385b
Juan Forero Galindo <66389957+jforerogalindo@users.noreply.github.com>
2022-10-07 13:14:34 -0500

    Precarga lista de usuarios

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,43 @@
-function cargar(name){
-    $("#contenido").load("pages/"+ name +".html");  
+function cargar(name) {
+  $("#contenido").load("pages/" + name + ".html");
 }
 
-async function load(url){
-    let consumos = loadJs(url + "/consumos.js");
+async function load(url) {
+  let consumos = loadJs(url + "/consumos.js");
 
-    await consumos;
+  await consumos;
 }
 
-
-function loadJs(url){
-    let script = document.createElement('script');
-    script.onload = function(){
-        console.log("Success loading", url);
+function loadJs(url) {
+  let promise = new Promise(function (resolve, reject) {
+    let script = document.createElement("script");
+    script.onload = function () {
+      console.log("Success loading", url);
+      resolve(true);
     };
-    script.onerror = function(){
-        console.log("Error loading", url);
+    script.onerror = function () {
+      console.log("Error loading", url);
+      reject(true);
     };
     script.src = url;
     document.head.appendChild(script);
+  });
+  return promise;
 }
 
-async function listaUsuarios(){
-    response = await userGetAll();
-    console.log(response);
-    for (const key in response) {
-        var newRowContent = "<tr><td scope=\"row\">"+ response[key].identification + "</td><td>" + response[key].name + "</td><td>" + response[key].rolId + "</td><td class=\"text-center\"><a class=\"text-warning\" data-bs-toggle=\"modal\" data-bs-target=\"#modalEditar\"><i class=\"fa-regular fa-pen-to-square\"></i></a></td><td class=\"text-center\"><a class=\"text-danger\"><i class=\"fa-regular fa-trash\"></i></a></td></tr>";
-        console.log(response[key]);
-        $("#userTableBody").append(newRowContent);
-    }
-    
+async function listaUsuarios() {
+  response = await userGetAll();
+  console.log(response);
+  for (const key in response) {
+    var newRowContent =
+      '<tr><td scope="row">' +
+      response[key].identification +
+      "</td><td>" +
+      response[key].name +
+      "</td><td>" +
+      response[key].rolId +
+      '</td><td class="text-center"><a class="text-warning" data-bs-toggle="modal" data-bs-target="#modalEditar"><i class="fa-regular fa-pen-to-square"></i></a></td><td class="text-center"><a class="text-danger"><i class="fa-regular fa-trash"></i></a></td></tr>';
+    console.log(response[key]);
+    $("#userTableBody").append(newRowContent);
+  }
 }
